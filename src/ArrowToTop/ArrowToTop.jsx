fe0c89c2a338fc5f2f56458db350ff9355a686b1
JoshuaNwaizu <joshuachris000@gmail.com>
2024-05-03 02:23:43 +0100

    New changes

--- src/ArrowToTop/ArrowToTop.jsx
+++ src/ArrowToTop/ArrowToTop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { IoArrowUpOutline } from 'react-icons/io5';
 import { motion } from 'framer-motion';
+import { Link } from 'react-scroll';
 
 const ArrowToTop = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,16 +16,24 @@ const ArrowToTop = () => {
 
   return (
     <div>
-      <motion.a
-        initial={{ y: isOpen && -250 }}
-        animate={{ y: isOpen && 0 }}
-        href=""
-        className={` fixed right-[1rem] inline-flex bottom-[-50%] font-bold text-xl text-[#2BD0D0] bg-[#FFF] p-3 shadow-md rounded-lg ${
-          isOpen ? 'bottom-[4rem] transition-all duration-300' : undefined
-        }`}
+      <Link
+        to="home"
+        spy={true}
+        smooth={true}
+        offset={-120}
+        duration={100}
       >
-        <IoArrowUpOutline />
-      </motion.a>
+        <motion.a
+          initial={{ y: isOpen && -250 }}
+          animate={{ y: isOpen && 0 }}
+          href=""
+          className={` fixed right-[1rem] inline-flex bottom-[-50%] font-bold text-xl text-[#2BD0D0] bg-[#FFF] p-3 shadow-md rounded-lg ${
+            isOpen ? 'bottom-[4rem] transition-all duration-300' : undefined
+          }`}
+        >
+          <IoArrowUpOutline />
+        </motion.a>
+      </Link>
     </div>
   );
 };
