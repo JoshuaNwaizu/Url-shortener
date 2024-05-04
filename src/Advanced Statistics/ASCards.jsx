092d720115735dea48081fc55fd0ed7f6c4bc20b
JoshuaNwaizu <joshuachris000@gmail.com>
2024-05-04 19:47:38 +0100

    New changes

--- src/Advanced Statistics/ASCards.jsx
+++ src/Advanced Statistics/ASCards.jsx
@@ -1,24 +1,24 @@
-import React from "react";
+import React from 'react';
 
 const statistics = [
   {
-    title: "Brand Recognition",
+    title: 'Brand Recognition',
     description:
-      "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
-    img: "/images/icon-brand-recognition.svg",
+      'Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.',
+    img: '/images/icon-brand-recognition.svg',
   },
   {
-    title: "Detailed Records",
+    title: 'Detailed Records',
     description:
-      "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
-    img: "/images/icon-detailed-records.svg",
+      'Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.',
+    img: '/images/icon-detailed-records.svg',
     line: true,
   },
   {
-    title: "Fully Customizable",
+    title: 'Fully Customizable',
     description:
-      "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
-    img: "/images/icon-fully-customizable.svg",
+      'Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.',
+    img: '/images/icon-fully-customizable.svg',
     line: true,
     top: true,
   },
@@ -26,7 +26,7 @@ const statistics = [
 
 const ASCards = () => {
   return (
-    <div className="mx-6 mt-24 mb-12  min-[768px]:mx-10 min-[820px]:mx-14 min-[884px]:mx-[5rem] min-[1024px]:mb-[10rem] min-[1280px]:mx-[8rem] min-[1400px]:mx-[12rem]">
+    <div className="mx-6 mt-24 mb-12  min-[768px]:mx-10 min-[820px]:mx-14 min-[884px]:mx-[5rem] min-[1024px]:mb-[10rem] min-[1280px]:mx-[8rem] min-[1300px]:mx-[12rem]">
       <div className="flex flex-col items-center justify-center gap-[6rem] min-[1024px]:flex-row min-[1024px]:gap-[2rem]">
         {statistics.map((item) => (
           <div key={item.description}>
@@ -34,43 +34,50 @@ const ASCards = () => {
               className={
                 item.line
                   ? `bg-[#2BD0D0] min-[1024px]:h-[8px] min-[1024px]:w-[2rem] min-[1024px]:translate-y-[9rem] min-[1024px]:translate-x-[-2rem] ${
-                      item.top && "min-[1024px]:translate-y-[10rem]"
+                      item.top && 'min-[1024px]:translate-y-[10rem]'
                     } `
                   : undefined
               }
             ></div>
             <div
               className={`bg-[#fff]  flex flex-col items-center justify-center px-7 py-10 rounded-lg min-[1024px]:px-3 ${
-                item.line ? "min-[1024px]:translate-y-10" : undefined
-              } ${item.top ? "min-[1024px]:translate-y-[4.5rem]" : undefined}`}
+                item.line ? 'min-[1024px]:translate-y-10' : undefined
+              } ${
+                item.top
+                  ? 'min-[1024px]:translate-y-[4.5rem] min-[1300px]:px-[1rem]'
+                  : undefined
+              }`}
             >
               <div
                 className={
                   item.line
-                    ? "flex flex-col items-center justify-center mt-[-3rem] translate-y-[-6rem] min-[1024px]:translate-y-[3rem] min-[1024px]:mt-[]"
+                    ? 'flex flex-col items-center justify-center mt-[-3rem] translate-y-[-6rem] min-[1024px]:translate-y-[3rem] min-[1024px]:mt-[]'
                     : undefined
                 }
               >
                 <div
                   className={
                     item.line
-                      ? "bg-[#2BD0D0] w-[8px] h-[9rem] translate-y-2 min-[1024px]:hidden "
+                      ? 'bg-[#2BD0D0] w-[8px] h-[9rem] translate-y-2 min-[1024px]:hidden '
                       : undefined
                   }
                 ></div>
 
                 <figure
                   className={`bg-[#3A3054] p-6 rounded-full -translate-y-[5rem] min-[1024px]:translate-x-[-5rem] min-[1024px]:p-5 ${
-                    item.top ? "min-[1024px]:p-4" : undefined
+                    item.top ? 'min-[1024px]:p-4' : undefined
                   }`}
                 >
-                  <img src={item.img} alt={item.name} />
+                  <img
+                    src={item.img}
+                    alt={item.name}
+                  />
                 </figure>
               </div>
 
               <div
                 className={`flex items-center justify-center flex-col gap-6 min-[1024px]:gap-3 min-[1024px]:items-start min-[1024px]:justify-start min-[1024px]:px-3 -mt-9  ${
-                  item.line ? "mt-[-8rem] min-[1024px]:mt-[1rem]" : undefined
+                  item.line ? 'mt-[-8rem] min-[1024px]:mt-[1rem]' : undefined
                 }`}
               >
                 <h1 className="text-2xl font-bold text-[#34313D] max-[375px]:text-xl  min-[768px]:text-3xl min-[1024px]:text-xl min-[1024px]:text-center">
--- src/URL form/LinkOutput.jsx
+++ src/URL form/LinkOutput.jsx
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
-import Button from "../Button";
-import { motion } from "framer-motion";
+import React, { useState } from 'react';
+import Button from '../Button';
+import { motion } from 'framer-motion';
 
 const LinkOutput = ({ getUrl, urlErr }) => {
   const [isCopied, setIsCopied] = useState([]);
 
   const handleCopyLink = (item) => {
     navigator.clipboard.writeText(item);
-    console.log("Copied:", item);
+    console.log('Copied:', item);
 
     setIsCopied((prevCopied) => [...prevCopied, item]);
 
@@ -16,10 +16,10 @@ const LinkOutput = ({ getUrl, urlErr }) => {
     }, 10000);
   };
   return (
-    <div className="mx-5 -translate-y-[6.5rem] flex flex-col min-[768px]:-translate-y-[5rem]  min-[768px]:mx-9 min-[820px]:mx-14  min-[884px]:mx-10 min-[884px]:justify-between min-[1024px]:mx-16 min-[1280px]:mx-[8.5rem] min-[1400px]:mx-[12rem]">
+    <div className="mx-5 -translate-y-[6.5rem] flex flex-col min-[768px]:-translate-y-[5rem]  min-[768px]:mx-9 min-[820px]:mx-14  min-[884px]:mx-10 min-[884px]:justify-between min-[1024px]:mx-16 min-[1280px]:mx-[8.5rem] min-[1300px]:mx-[12rem]">
       {getUrl.url.map((item) => (
         <motion.div
-          initial={{ y: "-100vh" }}
+          initial={{ y: '-100vh' }}
           animate={{ y: 0 }}
           className="bg-[#fff] flex flex-col py-5 px-5 gap-3 mb-7 rounded-lg  min-[768px]:items-center  min-[768px]:justify-center min-[884px]:flex-row  min-[884px]:justify-between  min-[1024px]:py-3 min-[1024px]:px-7  min-[1024px]:mb-4"
           key={item.secondInput + item.firstInput}
@@ -34,7 +34,7 @@ const LinkOutput = ({ getUrl, urlErr }) => {
           <div className="flex justify-center flex-col gap-3  min-[884px]:flex-row   min-[884px]:items-center ">
             <p
               className={`text-[1.2rem] tracking-[0.12px] font-medium leading-9 text-[#2BD0D0] ${
-                !item.secondInput && "hidden"
+                !item.secondInput && 'hidden'
               }`}
             >
               {item.secondInput}
@@ -45,7 +45,7 @@ const LinkOutput = ({ getUrl, urlErr }) => {
               </p>
             )}
             <motion.div
-              initial={{ x: "-100vw" }}
+              initial={{ x: '-100vw' }}
               animate={{ x: 0 }}
               whileHover={{
                 x: !item.secondInput ? [0, 10, -10, 10, -10, 10, 0] : 0,
@@ -60,12 +60,12 @@ const LinkOutput = ({ getUrl, urlErr }) => {
               className="flex items-center justify-center"
             >
               <Button
-                text={isCopied.includes(item.secondInput) ? "Copied!" : "Copy"}
+                text={isCopied.includes(item.secondInput) ? 'Copied!' : 'Copy'}
                 style={`bg-[#2BD0D0] py-3 max-[320px]:w-[15rem] w-[20rem]  min-[1024px]:w-[10rem] rounded-md text-[20px] font-bold text-[#fff] min-[768px]:w-[15rem] min-[884px]:w-[9rem] min-[1024px]:py-2 ${
-                  !item.secondInput && "opacity-40 "
+                  !item.secondInput && 'opacity-40 '
                 } ${
                   isCopied.includes(item.secondInput) &&
-                  "bg-[#3A3054] transition-all duration-150"
+                  'bg-[#3A3054] transition-all duration-150'
                 }`}
                 onHandleClick={() => handleCopyLink(item.secondInput)}
                 disabled={!item.secondInput && true}
